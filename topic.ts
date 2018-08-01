eee026847ccfba5a0e7f5579e4df7c9c8636bb05
lacyperk <40610308+lacyperk@users.noreply.github.com>
2018-08-01 03:42:00 -0500

    something

--- topic.ts
+++ topic.ts
@@ -1,7 +1,6 @@
 document.getElementById("enter").addEventListener("onclick", myFunction);
 
 function myFunction(){
-    var name = document.getElementById("fname").nodeName;
-    alert(name);
-    
+    //var name = document.getElementById("fname");
+    alert("somethign");
 }
