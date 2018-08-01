5fb739d606238b1f824fe15bb9ab1e8e5c4b15d0
lacyperk <40610308+lacyperk@users.noreply.github.com>
2018-08-01 03:15:58 -0500

    js ts

--- topic.js
+++ topic.js
@@ -1,4 +1,5 @@
 document.getElementById("enter").addEventListener("click", myFunction);
 function myFunction() {
-    document.getElementById("fname").innerHTML = "Hello," + "fname";
+    var name = document.getElementById("fname");
+    alert(name);
 }
--- topic.ts
+++ topic.ts
@@ -1,6 +1,7 @@
 document.getElementById("enter").addEventListener("click", myFunction);
 
 function myFunction(){
-    document.getElementById("fname").innerHTML = "Hello," + "fname";
+    var name = document.getElementById("fname");
+    alert(name);
     
 }
