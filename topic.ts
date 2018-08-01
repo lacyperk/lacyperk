d78ba5eaeedb4c9650582959104b5b79c6ad54b9
lacyperk <40610308+lacyperk@users.noreply.github.com>
2018-08-01 03:29:48 -0500

    blah

--- topic.ts
+++ topic.ts
@@ -1,8 +1,7 @@
 document.getElementById("enter").addEventListener("onclick", myFunction);
 
 function myFunction(){
-    var name = document.getElementById("fname");
-    var fname = name.nodeValue;
-    alert(fname);
+    var name = document.getElementById("fname").nodeName;
+    alert(name);
     
 }
